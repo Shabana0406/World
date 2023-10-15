713919adbd8d493b000b14128db6a851975eb646
Shabana0406 <135378209+Shabana0406@users.noreply.github.com>
2023-10-15 19:28:58 +0530

    Update vite.config.js
    resolve cors issue

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "https://shabana0406.github.io/city_data/city_data.json",
+    },
+  },
   base: "/World/",
   plugins: [react(), eslint()],
 });
